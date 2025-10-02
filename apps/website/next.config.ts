605e6c3c0847b20e17494f03b92e6030b3807156
Michael0520 <michael860520@gmail.com>
2025-10-03 05:44:49 +0800

    docs(website): add comment about baseConfig rewrites for future maintenance

--- apps/website/next.config.ts
+++ apps/website/next.config.ts
@@ -5,6 +5,8 @@ import {baseConfig} from '@repo/next-config/base';
 const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts');
 
 // PostHog reverse proxy configuration to prevent ad-blocker blocking
+// Note: baseConfig currently has no rewrites, so we can safely define them here
+// If baseConfig adds rewrites in the future, this will need to be updated to merge them
 const config: NextConfig = {
   ...baseConfig,
   skipTrailingSlashRedirect: true,
